ef233518cef020377a86d745fdc190fc6dd494a8
xhafan <xhafan@gmail.com>
2018-11-16 15:38:20 +0100

    imo number is not valid by default

--- src/LegacyWebFormsApp/WebFormsAdoNet/CreateShip.aspx.cs
+++ src/LegacyWebFormsApp/WebFormsAdoNet/CreateShip.aspx.cs
@@ -13,12 +13,12 @@ namespace LegacyWebFormsApp.WebFormsAdoNet
             var shipName = ShipNameTextBox.Text;
             var tonnage = decimal.Parse(TonnageTextBox.Text);
             var imoNumber = ImoNumberTextBox.Text;
-
-            const int isImoNumberValid = 0;
-
+            
             SqlCommandExecutor.ExecuteSqlCommand(cmd =>
             {
-                const int hasImoNumberBeenVerified = 1;
+                const int hasImoNumberBeenVerified = 0;
+                const int isImoNumberValid = 0;
+
                 cmd.CommandText = 
                     $"EXEC CreateShip '{shipName}', {tonnage}, '{imoNumber}', {hasImoNumberBeenVerified}, {isImoNumberValid}";
                 var shipId = (int)cmd.ExecuteScalar();
